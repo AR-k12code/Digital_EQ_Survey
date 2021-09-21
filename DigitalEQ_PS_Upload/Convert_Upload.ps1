277adf814deafacde847a07352b82bb332df1d03
Charlie Weber <charlie.weber@rpsar.net>
2021-09-21 10:56:06 -0500

    First draft of PS code

--- DigitalEQ_PS_Upload/Convert_Upload.ps1
+++ DigitalEQ_PS_Upload/Convert_Upload.ps1
@@ -1,19 +1,56 @@
 #Simple file to output the Google sheet information to format needed for upload into eschool.
 
-$csvloc = ""
+$csvloc = "c:\scripts\Digital EQ Survey (Responses) - Form Responses 1.csv" #If you are using the default name and Google form.
 $students = Import-Csv $csvloc
 $rows =@()
-$district ="405000"
-$outputfileloc = ""
+$district ="405000" #Use number from here https://adedata.arkansas.gov/LEA/Home/districts if you have a leading zero drop it
+$outputfileloc = "c:\scripts\DigEQ.csv" #Default save location
+$ADstudentID = "EmployeeNumber" #Ad field that contains StudentID
+$StuIdLength = "6" #Change to your districts default student ID length (shortest possible number the student ID should be)
+$StuIdLeadingNumber = "" #Here for future use; not being used currently
+$DefaultInternet = "N" #View ReadMe for specifics for valid answers.
+$DefaultInstructionalOption = "1" #View ReadMe for specifics for valid answers.
+$DistrictProvided = "DP" #Default entry for Deive source if the answer is left blank: DP,OT,PL only options
+$DefaultLearnDevice = "4" #View ReadMe for specifics for valid answers.
+$DefaultSharedDevice = "N" #View ReadMe for specifics for valid answers.
+$DefaultDeviceSerialNumber = "NotProvided" #View ReadMe for specifics for valid answers.
+$DefaultInternetType = "9" #Set to unknown if this is not provided, view readme for valid answers
+$DefaultInternetPerformance = "N" #View ReadMe for specifics for valid answers.
+$DefaultInternetBarrier = "NA" #View ReadMe for specifics for valid answers.
 
+function RemoveSpecials ([String]$in)
+{
+ $in = $in -replace("[^0-9]","")
+ return $in
+}
 
 
 Foreach ($st in $students){
-    $stid = $st.StudentID
+    $stid = RemoveSpecials($st.StudentID)
+if ($stid.length -lt $StuIdLength){
+    $stid = (Get-ADUser ($st.'Email Address').Split("@")[0] -Properties $ADstudentID).$ADstudentID
+}
+
+
     Write-host Creating rows for $stid
-    For ($i=1;$i -le 10;$i++){
-    $stq = $stu.psobject.properties.name[$i]
-    $stan = $stu.psobject.properties.value[$i]
+    For ($i=3;$i -le 11;$i++){
+    $stq = ($stu.psobject.properties.value[$i]).split("|")[0].trim()
+    $stan = ($stu.psobject.properties.value[$i]).split("|")[1].trim()
+    if ($stan = " "){
+        Write-Host "Blank answer using default value for $stq"
+        Switch ($stq){
+            "38" {$stan = $DefaultInternet}
+            "39" {$stan = $DefaultInstructionalOption}
+            "40" {$stan = $DistrictProvided}
+            "42" {$stan = $DefaultLearnDevice}
+            "43" {$stan = $DefaultSharedDevice}
+            "44" {$stan = ($DefaultDeviceSerialNumber+$stid)}
+            "45" {$stan = $DefaultInternetType}
+            "46" {$stan = $DefaultInternetPerformance}
+            "47" {$stan = $DefaultInternetBarrier}
+        Default {$stan = $stan}
+        }
+    }
     $rows += "$district,$stid,102,$stq,1,$stan"
     }
     Write-host "Finished rows for $stid"
